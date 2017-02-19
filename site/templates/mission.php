5dd039529cccace5dfd2bc74b163c74afd20eec3
Harvy <harvinder@harvinderatwal.com>
2017-02-19 16:30:38 +0000

    Changed Readme, added mission template

---
+++ site/templates/mission.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <head>
+        <title><?php snippet('common/head_title') ?></title>
+
+        <?php snippet('common/head_meta') ?>
+
+        <?php snippet('common/head_js') ?>
+
+        <?php snippet('common/head_css') ?>
+    </head>
+    <body class="<?php snippet('common/body_class') ?>">
+        <!-- Wrapper -->
+        <div id="wrapper">
+
+            <?php snippet('common/header') ?>
+
+            <!-- Main -->
+            <div id="main">
+                <div class="inner">
+                    <h1><?php echo $page->title() ?></h1>
+
+                    <section class="gallery">
+                      <?php foreach($page->images() as $image): ?>
+                          <figure>
+                            <a href="<?php echo $image->url() ?>">
+                              <img src="<?php echo $image->url() ?>" alt="">
+                            </a>
+                          </figure>
+                      <?php endforeach ?>
+                    </section>
+
+                    <?php echo $page->text()->kirbytext() ?>
+                </div>
+            </div>
+
+            <?php snippet('common/footer') ?>
+
+        </div>
+
+        <?php snippet('common/foot_js') ?>
+
+    </body>
+</html>
